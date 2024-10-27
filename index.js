620ee1b04f462e97e64f66ce90358c11f5298b96
kevin young <you.k2208@gmail.com>
2024-10-26 18:47:42 -0700

    added hemiLight, and wireMesh

--- index.js
+++ index.js
@@ -17,3 +17,29 @@ camera.position.z = 2;
 
 //scene
 const scene = new THREE.Scene();
+
+const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const mat = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  flatShading: true,
+});
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
+scene.add(hemiLight);
+
+const wireMat = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+});
+
+const wireMesh = new THREE.Mesh(geo, wireMat);
+scene.add(wireMesh);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
