b6a8daaf25b02714e3a7921f7ba134d768269ba3
kevin young <you.k2208@gmail.com>
2024-10-26 17:27:44 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import * as THREE from "three";
+
+// Renderer
+const w = window.innerWidth;
+const h = window.innerHeight;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+// Camera
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+//scene
+const scene = new THREE.Scene();
